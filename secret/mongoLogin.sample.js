916b03b6e1051c5c86be8eb6a059ba014b16f4f9
Ludo Loops <ludoloops@prontonmail.com>
2020-07-13 16:07:26 +0200

    feat: ajout fichier sample

---
+++ secret/mongoLogin.sample.js
@@ -0,0 +1,5 @@
+// put your mongodb url and credential as follow
+// then rename the file : mongoLogin.js
+
+module.exports =
+  'mongodb+srv://<username>:<password>@cluster0-x4pis.mongodb.net/<dbname>?retryWrites=true&w=majority';
---
+++ secret/tokenKey.js
@@ -0,0 +1,3 @@
+// salt sample syntax, don't forget to make your own !!!
+
+module.exports = 'X37h2v$o6AErY%yTGJdjmds';
