ec2a9f1d3a0e34bdc79a3bd5b20b9cc5964d12c0
Ludo Loops <ludoloops@prontonmail.com>
2020-07-13 13:45:39 +0200

    Fix: modification image

--- routes/sauce.js
+++ routes/sauce.js
@@ -8,7 +8,7 @@ const router = express.Router();
 router.get('/', auth, sauceCtrl.getAllSauce);
 router.post('/', auth, multer, sauceCtrl.createSauce);
 router.get('/:id', auth, sauceCtrl.getOneSauce);
-router.put('/:id', auth, sauceCtrl.modifySauce);
+router.put('/:id', auth, multer, sauceCtrl.modifySauce);
 router.delete('/:id', auth, sauceCtrl.deleteSauce);
 router.post('/:id/like', auth, sauceCtrl.like);
 
