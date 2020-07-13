f62ef63b265afdc6c24068fe6539e9b7faed2c7b
Ludo Loops <ludoloops@prontonmail.com>
2020-07-13 15:12:54 +0200

    feat: delete ancienne image

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -41,12 +41,21 @@ exports.modifySauce = (req, res) => {
         }`
       }
     : { ...req.body };
-  Sauce.updateOne(
-    { _id: req.params.id },
-    { ...sauceObject, _id: req.params.id }
-  )
-    .then(() => res.status(200).json({ message: 'Objet modifié !' }))
-    .catch((error) => res.status(400).json({ error }));
+  console.log(sauceObject);
+
+  Sauce.findOne({ _id: req.params.id })
+    .then((sauce) => {
+      const filename = sauce.imageUrl.split('/images')[1];
+      fs.unlink(`images/${filename}`, () => {
+        Sauce.updateOne(
+          { _id: req.params.id },
+          { ...sauceObject, _id: req.params.id }
+        )
+          .then(() => res.status(200).json({ message: 'Objet modifié !' }))
+          .catch((error) => res.status(400).json({ error }));
+      });
+    })
+    .catch((error) => res.status(500).json({ error }));
 };
 
 exports.deleteSauce = (req, res) => {
